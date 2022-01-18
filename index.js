47c8a7e0fcb1abfa71f57abcd9cfe6927188a18b
Eoin Carrick <22carri99@gmail.com>
2022-01-18 13:53:59 -0800

    coomit

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-let randomNum = Math.floor(Math.random() * 100 + 1);
+let randomNum = Math.floor(Math.random() * 100 + 1); // numbers from 1 to 100
 console.log(randomNum);
+
+const guess = document.querySelector(".guess");
+const lastResult = document.querySelector(".lastResult");
+const lowOrHigh = document.querySelector(".lowOrHigh");
+
+const guessSubmit = document.querySelector(".guessSubmit");
+const guessField = document.querySelector(".guessField");
+
+let guessCount = 1;
+let resetButton;
+
+function checkGuess() {
+  alert(`I'm a placeholder`);
+}
